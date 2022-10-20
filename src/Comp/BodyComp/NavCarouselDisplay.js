e63ff4d72b67ffd9db6564fbaf36dea033d2962e
BaronCoding <mikegbaron@gmail.com>
2022-10-20 14:53:46 +0300

    update
    Working Carousel

--- src/Comp/BodyComp/NavCarouselDisplay.js
+++ src/Comp/BodyComp/NavCarouselDisplay.js
@@ -1,38 +1,34 @@
-import React, { useEffect } from 'react';
+import React, { useState } from 'react';
 import '../Styles/NavCarouselStyle.css';
 
 export default function NavDisplay() {
-  const carList = [
-    { active: false, displayed: 1 },
-    { active: true, displayed: 2 },
-    { active: false, displayed: 3 },
-    { active: false, displayed: 4 }
-  ];
-
-  // useEffect(() => {
-  //   displayList();
+  const [carList, setCarList] = useState([
+    { active: false, displayed: 1, name: 'liat', item: 'item_1' },
+    { active: true, displayed: 2, name: 'eithan', item: 'item_2' },
+    { active: false, displayed: 3, name: 'emma', item: 'item_3' },
+    { active: false, displayed: 4, name: 'mike', item: 'item_4' }
+  ]);
   
-  // }, [carList])
-  
-  const changeActive = (ap) => {
-    carList.map((e,i) => {
-      ap === i ? e.active = true : e.active = false;
-    })
-    console.log(carList);
+  const changeActive = (index) => {
+    let arr = carList.filter((e,i) => i !== index);
+    let line = carList[index];
+    arr.unshift(line);
+    arr.forEach((e,i) => { e.displayed = i+1; e.item = `item_${i+1}` });
+    setCarList((arr) => [...arr]);
+    
   };
 
   return (
     <div className='carouselContainer'>
-        <div className='list'>
-            {carList.map((e, i) => {
+        <div className='carousel carList' >
+          {carList.map((e, i) => {
               return (
-          // <li key={ i } className={ e.active ? `carousel item_${ e.displayed } ${ e.active }` : `carousel item_${ e.displayed }`}  onClick={ () => changeActive(i) }>
-          <div key={ i } className={ `carousel item_${ e.displayed }`}  id={ `${e.active}`} onClick={ () => changeActive(i) }>
-          { e.displayed }
-          </div>
-          )
-          
-        })  }
+                // <div key={ i } className={ e.active ? `carousel item_${ e.displayed } ${ e.active }` : `carousel item_${ e.displayed }`}  onClick={ () => changeActive(i) }>
+                <div key={ i } className={ e.item } id='carousel_item' onClick={ () => changeActive(i) }>
+                  { e.name }
+                </div>
+              ) 
+        })};
         </div>
     </div>
   )
