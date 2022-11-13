eb8b86f0558dffc3b8e07c901eaac7fc4f336c24
BaronCoding <mikegbaron@gmail.com>
2022-11-13 13:10:59 +0200

    0.7

--- src/Comp/NavComp/About.js
+++ src/Comp/NavComp/About.js
@@ -1,20 +1,26 @@
 import React from 'react';
 
-// import './BodyStyle.css';
+import '../Styles/ContentStyling.css';
 
 export default function About() {
   return (
-    <div className='introDivStyle'>
-      <p>I'm Michael Baron</p>
-      <p>
-        Discovered the passion for coding in SVCollege
-        and been learing by myself and creating different 
-        projects.
-      </p>
-      <p>
-        Completed frontend course through SVCollege in 2020. 
-        After internship perioed with BeSt continued working on my own projects and developing   
-      </p>
-    </div>
+    <>
+      <div className='cornerBorderLeftTop'></div>
+      <div className='cornerFilerLeftTop'></div>
+      <div className='cornerBorderRigthBottom'></div>
+      <div className='cornerFilerRigthBottom'></div>
+      <main className='introDivStyle'>
+        <p>I'm Michael Baron</p>
+        <p>
+          Discovered the passion for coding in SVCollege
+          and been learing by myself and creating different 
+          projects.
+        </p>
+        <p>
+          Completed frontend course through SVCollege in 2020. 
+          After internship perioed with BeSt continued working on my own projects and developing   
+        </p>
+      </main>
+    </>
   )
 }
